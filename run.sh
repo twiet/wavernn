91a1a861b16623064415c0223716662d40627229
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-04-06 19:17:33 +0000

    check

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 wget http://download.magenta.tensorflow.org/datasets/nsynth/nsynth-train.jsonwav.tar.gz
 mkdir data
-tar -xvzf nsynth-test.jsonwav.tar.gz -C ./data
-python3 preprocess.py ./data/nsynth/train/audio
+tar -xvzf nsynth-train.jsonwav.tar.gz -C ./data
+python3 preprocess.py ./data/nsynth/wav/audio
 python3 train.py ./data_dir/
